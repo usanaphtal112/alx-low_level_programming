0da92632323f46fc53f3cdf249bce42824de60f3
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2023-07-12 04:38:28 -0700

    C - argc, argv update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,9 +16,10 @@ int _atoi(char *s)
 	len = 0;
 	f = 0;
 	digit = 0;
-
 	while (s[len] != '\0')
+	{
 		len++;
+	}
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
@@ -39,4 +40,27 @@ int _atoi(char *s)
 	if (f == 0)
 		return (0);
 	return (n);
+}
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
+	return (0);
+
 }
