14951f12bfa4cf12e530c98386cbbbc77462d89c
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2023-07-20 13:03:00 -0700

    Function pointers updates

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -5,7 +5,7 @@
  * struct op - A struct op.
  * @op: The operator.
  * @f: The associated function.
- */
+*/
 
 typedef struct op
 {
@@ -13,11 +13,13 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+
 
 #endif
