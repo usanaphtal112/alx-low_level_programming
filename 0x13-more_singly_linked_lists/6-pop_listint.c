11e788727e7de777ade571dcc6436b020e24fe16
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2023-07-31 23:04:58 -0700

    Updated more on signle linked lists up to task 8

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,21 +1,25 @@
 #include "lists.h"
 
 /**
- * pop_listint - Deletes the head node of a listint_t linked list.
- * @head: Double pointer to the head of the list.
- * Return: The data (n) of the deleted head node, or 0 if the list is empty.
+ * pop_listint - deletes the head node of a linked list
+ * @head: pointer to the first element in the linked list
+ *
+ * Return: the data inside the elements that was deleted,
+ * or 0 if the list is empty
  */
 
 int pop_listint(listint_t **head)
 {
-	if (head == NULL || *head == NULL) /* Check if the list is empty.*/
+	listint_t *temp;
+	int num;
+
+	if (!head || !*head)
 		return (0);
 
-	listint_t *temp = *head;
-	int data = temp->n;
+	num = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
 
-	*head = temp->next;
-	free(temp);
-
-	return (data);
+	return (num);
 }
