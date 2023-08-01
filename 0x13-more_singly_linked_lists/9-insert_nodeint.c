3f96c49dbd0ce3d093697736f5d6a1a6fa39339b
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2023-07-31 23:09:18 -0700

    Updated more on signle linked lists up to task 8

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,42 +1,44 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - Inserts a new node at a given position in a listint_t list.
+ * insert_nodeint_at_index - Inserts a new node.
  * @head: Double pointer to the head of the list.
  * @idx: Index of the list where the new node should be added (starting at 0).
  * @n: Integer value for the new node.
  * Return: The address of the new node, or NULL if it failed.
  */
-listint_t *insert_nodeint_at_index(
-listint_t **head, unsigned int idx, int n
-);
-{
-	listint_t *new_node = malloc(sizeof(listint_t));
-	listint_t *current = *head;
-	unsigned int count = 0;
 
-	if (new_node == NULL)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *new;
+	listint_t *temp = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
 		return (NULL);
 
-	new_node->n = n;
-	new_node->next = NULL;
+	new->n = n;
+	new->next = NULL;
 
 	if (idx == 0)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
-	while (current != NULL)
+
+	for (i = 0; temp && i < idx; i++)
 	{
-		if (count == idx - 1)
-			new_node->next = current->next;
-			current->next = new_node;
-			return (new_node);
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
 		}
-		current = current->next;
-		count++;
+		else
+			temp = temp->next;
 	}
-	free(new_node);
+
 	return (NULL);
 }
