caa3d599d54e4ffd6cec9ae544576ac434abc56e
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2023-07-31 23:53:28 -0700

    Advanced tasks, more on single linked list 11 task to 14 task

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,56 +1,6 @@
 #include "lists.h"
 
 /**
- * find_loop - Detects the loop in a listint_t linked list.
- * @head: Pointer to the head of the list.
- * Return: A pointer to the node where the loop starts, or NULL if no loop.
- */
-
-listint_t *find_loop(listint_t *head)
-{
-	listint_t *slow, *fast;
-
-	slow = head;
-	fast = head;
-
-	while (fast != NULL && fast->next != NULL)
-	{
-		slow = slow->next;
-		fast = fast->next->next;
-
-		if (slow == fast)
-			return (slow);
-	}
-
-	return (NULL);
-}
-
-/**
- * free_nodes_after_loop - Frees the nodes after the loop
- * @head: Pointer to the head of the list.
- * @loop_node: Pointer to the node where the loop starts.
- */
-
-void free_nodes_after_loop(listint_t *head, listint_t *loop_node)
-{
-	listint_t *temp;
-	size_t size = 0;
-
-	do {
-		size++;
-		loop_node = loop_node->next;
-	} while (loop_node != head);
-
-	while (size > 0)
-	{
-		temp = head;
-		head = head->next;
-		free(temp);
-		size--;
-	}
-}
-
-/**
  * free_listint_safe - Frees a listint_t linked list safely.
  * @h: Double pointer to the head of the list.
  * Return: The size of the list that was freed.
@@ -58,36 +8,33 @@ void free_nodes_after_loop(listint_t *head, listint_t *loop_node)
 
 size_t free_listint_safe(listint_t **h)
 {
-	size_t size = 0;
-	listint_t *loop_node, *temp;
+	size_t len = 0;
+	int diff;
+	listint_t *temp;
 
-	loop_node = find_loop(*h);
+	if (!h || !*h)
+		return (0);
 
-	if (loop_node != NULL)
+	while (*h)
 	{
-		/* Free nodes before the loop */
-		size = 0;
-		while (*h != loop_node)
+		diff = *h - (*h)->next;
+		if (diff > 0)
 		{
-			temp = *h;
-			*h = (*h)->next;
-			free(temp);
-			size++;
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			len++;
 		}
-
-		free_nodes_after_loop(*h, loop_node);
-	}
-	else
-	{
-		/* No loop, free the whole list */
-		while (*h != NULL)
+		else
 		{
-			temp = *h;
-			*h = (*h)->next;
-			free(temp);
-			size++;
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
 		}
 	}
 
-	return (size);
+	*h = NULL;
+
+	return (len);
 }
