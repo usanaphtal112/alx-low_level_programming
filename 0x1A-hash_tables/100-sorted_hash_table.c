0801bdb76cff4b66851265ac339c950743475559
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2023-12-21 10:33:08 -0800

    Modified file

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -10,15 +10,7 @@ void shash_table_print_rev(const shash_table_t *ht);
 /**
  * shash_table_delete - Deletes a sorted hash table and its contents.
  *
- * This function deallocates the memory used
- * by the given sorted hash table,
- * including all key-value pairs stored in it.
- * It frees the memory occupied by
- * each node in the hash table and the array itself.
- * After calling this function, the sorted hash
- * table should not be accessed.
- *
- * @param ht    A pointer to the sorted hash table to be deleted.
+ * @ht: A pointer to the sorted hash table to be deleted.
  */
 void shash_table_delete(shash_table_t *ht)
 {
@@ -45,13 +37,9 @@ void shash_table_delete(shash_table_t *ht)
 /**
  * shash_table_create - Creates a sorted hash table.
  *
- * This function allocates memory for a new sorted hash table and initializes
- * its components, including the array of pointers, head, and tail for sorted
- * access. The size parameter determines the initial size of the array.
+ * @size: The initial size of the array in the sorted hash table.
  *
- * @param size  The initial size of the array in the sorted hash table.
- *
- * @return      A pointer to the created sorted hash table, or NULL on failure
+ * Return: A pointer to the created sorted hash table, or NULL on failure.
  */
 shash_table_t *shash_table_create(unsigned long int size)
 {
@@ -75,19 +63,14 @@ shash_table_t *shash_table_create(unsigned long int size)
 }
 
 /**
- * shash_table_get - Retrieve the value associated
- * with a key in a sorted hash table.
+ * shash_table_get - Retrieve the value associated with
+ * a key in a sorted hash table.
  *
- * This function searches for the specified key in the
- * given sorted hash table and returns the corresponding
- * value if the key is found.
- * If the key is not present in the hash table, NULL is returned.
+ * @ht: A pointer to the sorted hash table from which to retrieve the value.
+ * @key: The key for which the associated value is to be retrieved.
  *
- * @param ht    A pointer to the sorted hash table from which
- * @param key   The key for which the associated value is to be retrieved.
- *
- * @return      Returns a pointer to the value associated with the key
- *              the key is not found or if ht or key is NULL.
+ * Return: Returns a pointer to the value associated with
+ * the key or NULL if not found.
  */
 char *shash_table_get(const shash_table_t *ht, const char *key)
 {
@@ -111,18 +94,11 @@ char *shash_table_get(const shash_table_t *ht, const char *key)
 /**
  * shash_table_set - Adds or updates a key-value pair in a sorted hash table.
  *
- * This function sets a key-value pair in the given sorted hash table.
- * If the key already exists in the hash table, the
- * corresponding value is updated. If the key is not present,
- * a new entry is created for the key-value pair
- * and the sorted order is maintained based on the keys.
+ * @ht: A pointer to the sorted hash table where the key-value pair will be
+ * @key: The key to be added or updated in the sorted hash table.
+ * @value: The value associated with the key to be set.
  *
- * @param ht    A pointer to the sorted hash table
- * where the key-value pair will be set.
- * @param key   The key to be added or updated in the sorted hash table.
- * @param value The value associated with the key to be set.
- *
- * @return      Returns 1 on success, 0 on failure
+ * Return: Returns 1 on success, 0 on failure.
  */
 int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 {
@@ -201,11 +177,7 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 /**
  * shash_table_print - Prints the content of a sorted hash table.
  *
- * This function prints the key-value pairs stored in the given sorted
- * The output is formatted as a JSON-like representation enclosed
- * If the hash table is empty, an empty set of curly braces is printed.
- *
- * @param ht    A pointer to the sorted hash table to be printed.
+ * @ht: A pointer to the sorted hash table to be printed.
  */
 void shash_table_print(const shash_table_t *ht)
 {
@@ -227,13 +199,10 @@ void shash_table_print(const shash_table_t *ht)
 }
 
 /**
- * shash_table_print_rev - Prints the content of a sorted hash table.
+ * shash_table_print_rev - Prints the content of a sorted
+ * hash table in reverse order.
  *
- * This function prints the key-value pairs stored in the given sorted.
- * in reverse order. The output is formatted as a JSON-like representation
- * in curly braces. If the hash table is empty, an empty set of curly braces.
- *
- * @param ht    A pointer to the sorted hash table to be printed in reverse.
+ * @ht: A pointer to the sorted hash table to be printed in reverse.
  */
 void shash_table_print_rev(const shash_table_t *ht)
 {
