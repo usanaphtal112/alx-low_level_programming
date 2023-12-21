d22526de3a5fdcfb607a1c522a4e619c5be3228e
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2023-12-21 09:44:50 -0800

    Added changes for task 2

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,5 +1,4 @@
 #include "hash_tables.h"
-
 /**
  * hash_djb2 - DJB2 hash algorithm for strings.
  *
@@ -15,13 +14,13 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
